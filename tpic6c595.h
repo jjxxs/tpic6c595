0b753a738069b1e960a80f90c52fa15759ff4355
Joscha Behrmann <behrmann@volke.de>
2020-04-21 13:25:34 +0200

    Added some documentation

--- tpic6c595.h
+++ tpic6c595.h
@@ -28,10 +28,22 @@
 #define TPIC6C595_MODE_BTN_CLEAR    3
 
 /**
- * Sets the write delay
+ * Sets the bitbang delay, e.g. the time to wait between state-changes of
+ * the gpio-line in microseconds.
  */
 #define TPIC6C595_IOCTL_BITBANG_DELAY _IO(0xaa, 0x01)
+
+/**
+ * Sets the effect-duration of the button in milliseconds. E.g. the time in milliseconds
+ * between flashes when the button is configured to flash the device.
+ */
 #define TPIC6C595_IOCTL_EFFECT_DURATION _IO(0xaa, 0x02)
+
+/**
+ * Sets the drivers assumed size of the the devices in bytes. E.g. one single
+ * TPIC6C595 is one byte long but they can be daisy-chained so two tpics
+ * will be two bytes long etc.
+ */
 #define TPIC6C595_IOCTL_SIZE _IO(0xaa, 0x03)
 
 #endif //TPIC6C595_TPIC6C595_H
